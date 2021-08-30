7b22198737a7bdaf256c1b872614e63253ce9028
younoah <kr_uno_10@naver.com>
2021-08-31 00:22:54 +0900

    feat: navbar, documents list 중첩구조 재귀로 렌더링 구현

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -11,6 +11,20 @@ export default function Navbar({ $target, initialState }) {
   $target.append($navbar);
   $navbar.addEventListener('click', ({ target }) => {});
 
+  const renderDocumentList = function recur(document) {
+    const childDocuments = document.documents;
+
+    return /* html */ `
+      <ul class="navbar__list">
+        <li data-id="${document.id}" class="navbar__list__document">
+          ${document.title} <button class="add-document-button">추가</button
+          ><button class="delete-document-button">삭제</button>
+        </li>
+        ${childDocuments.map(document => recur(document)).join('')}
+      </ul>
+    `;
+  };
+
   this.setState = () => {};
 
   this.render = () => {
@@ -18,18 +32,7 @@ export default function Navbar({ $target, initialState }) {
       <div class="navbar__user">
         <h3 id="user-title">유저이름</h3>
       </div>
-      ${this.state
-        .map(
-          document => /* html */ `
-            <ul class="navbar__list">
-              <li data-id="${document.id}" class="navbar__list__document">
-                ${document.title} <button class="add-document-button">추가</button
-                ><button class="delete-document-button">삭제</button>
-              </li>
-            </ul>
-          `
-        )
-        .join('')}
+      ${this.state.map(document => renderDocumentList(document)).join('')}
     `;
   };
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,26 +5,49 @@ import App from './components/App.js';
 const navbarDummy = [
   {
     id: 1,
-    title: '루트문서1',
+    title: '문서1',
     documents: [],
     createdAt: '2021-08-29T14:25:55.606Z',
     updatedAt: '2021-08-29T14:25:55.611Z',
   },
   {
     id: 2,
-    title: '루트문서1',
+    title: '문서2',
     documents: [
       {
         id: 4,
-        title: '루트문서1',
+        title: '문서4',
         documents: [],
         createdAt: '2021-08-29T14:25:55.606Z',
         updatedAt: '2021-08-29T14:25:55.611Z',
       },
       {
         id: 5,
-        title: '루트문서1',
-        documents: [],
+        title: '문서5',
+        documents: [
+          {
+            id: 7,
+            title: '문서7',
+            documents: [],
+            createdAt: '2021-08-29T14:25:55.606Z',
+            updatedAt: '2021-08-29T14:25:55.611Z',
+          },
+          {
+            id: 8,
+            title: '문서8',
+            documents: [
+              {
+                id: 9,
+                title: '문서9',
+                documents: [],
+                createdAt: '2021-08-29T14:25:55.606Z',
+                updatedAt: '2021-08-29T14:25:55.611Z',
+              },
+            ],
+            createdAt: '2021-08-29T14:25:55.606Z',
+            updatedAt: '2021-08-29T14:25:55.611Z',
+          },
+        ],
         createdAt: '2021-08-29T14:25:55.606Z',
         updatedAt: '2021-08-29T14:25:55.611Z',
       },
@@ -34,11 +57,11 @@ const navbarDummy = [
   },
   {
     id: 3,
-    title: '루트문서1',
+    title: '문서3',
     documents: [
       {
         id: 6,
-        title: '루트문서1',
+        title: '문서6',
         documents: [],
         createdAt: '2021-08-29T14:25:55.606Z',
         updatedAt: '2021-08-29T14:25:55.611Z',
